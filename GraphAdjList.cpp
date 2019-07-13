16b3c21c72f403846a1a510a980edf8e8e68ab61
Tousif Anan <1705053@ugrad.cse.buet.ac.bd>
2019-07-14 00:10:39 +0600

    List done

--- GraphAdjList.cpp
+++ GraphAdjList.cpp
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<iostream>
 #define NULL_VALUE -999999
 #define INFINITY 999999
 #define WHITE 1
 #define GREY 2
 #define BLACK 3
+
+using namespace std;
 
 class Queue
 {
@@ -187,7 +190,7 @@ void ArrayList::printList()
     int i;
     for(i=0;i<length;i++)
         printf("%d ", list[i]);
-    printf("Current size: %d, current length: %d\n", listMaxSize, length);
+  //  printf("Current size: %d, current length: %d\n", listMaxSize, length);
 }
 
 ArrayList::~ArrayList()
@@ -204,6 +207,7 @@ class Graph
 	int nVertices, nEdges ;
 	bool directed ;
 	ArrayList  * adjList ;
+	int *color,*parent,*dist;
 	//define other variables required for bfs such as color, parent, and dist
 	//you must use pointers and dynamic allocation
 
@@ -214,7 +218,7 @@ public:
 	void addEdge(int u, int v);
 	void removeEdge(int u, int v);
 	bool isEdge(int u, int v);
-    int getDegree(int u);
+    int getDegree(int u,bool out);
     void printAdjVertices(int u);
     bool hasCommonAdjacent(int u, int v);
     int getDist(int u, int v);
@@ -231,6 +235,7 @@ Graph::Graph(bool dir)
 	adjList = 0 ;
 	directed = dir ; //set direction of the graph
 	//define other variables to be initialized
+
 }
 
 void Graph::setnVertices(int n)
@@ -238,6 +243,9 @@ void Graph::setnVertices(int n)
 	this->nVertices = n ;
 	if(adjList!=0) delete[] adjList ; //delete previous list
 	adjList = new ArrayList[nVertices] ;
+	color = new int[nVertices];
+	parent = new int[nVertices];
+	dist = new int[nVertices];
 }
 
 void Graph::addEdge(int u, int v)
@@ -251,26 +259,74 @@ void Graph::addEdge(int u, int v)
 void Graph::removeEdge(int u, int v)
 {
     //write this function
+    if(u<0 || v<0 || u>=nVertices || v>=nVertices) return; //vertex out of range
+    adjList[u].removeItem(v);
+    if(!directed) adjList[v].removeItem(u);
+    this->nEdges--;
+
 }
 
 bool Graph::isEdge(int u, int v)
 {
     //returns true if (u,v) is an edge, otherwise should return false
+    if(u<0 || v<0 || u>=nVertices || v>=nVertices) return false; //vertex out of range
+
+    if(adjList[u].searchItem(v)!=NULL_VALUE)
+        return true;
+    else
+        return false;
 }
 
-int Graph::getDegree(int u)
+int Graph::getDegree(int u,bool out = true)
 {
     //returns the degree of vertex u
+    if(!directed)
+        return adjList[u].getLength();
+    int len = 0;
+    if(out)
+    {
+        len += adjList[u].getLength();
+    }
+    else
+    {
+        for(int i =0;i<nVertices;i++)
+        {
+            if(isEdge(i,u))
+                len++;
+        }
+    }
+
+    return len;
 }
 
 void Graph::printAdjVertices(int u)
 {
     //prints all adjacent vertices of a vertex u
+    if(!directed)
+        adjList[u].printList();
+    else
+    {
+        adjList[u].printList();
+        for(int i=0;i<nVertices;i++)
+        {
+            if(isEdge(i,u))
+                printf("%d ",i);
+        }
+    }
 }
 
 bool Graph::hasCommonAdjacent(int u, int v)
 {
     //returns true if vertices u and v have common adjacent vertices
+    for(int i=0;i<adjList[u].getLength();i++)
+    {
+        for(int j = 0;j<adjList[v].getLength();j++)
+        {
+            if(adjList[u].getItem(i)==adjList[v].getItem(j))
+                return true;
+        }
+    }
+    return false;
 
 }
 
@@ -278,6 +334,33 @@ void Graph::bfs(int source)
 {
     //complete this function
     //initialize BFS variables for all n vertices first
+    for(int i=0;i<nVertices;i++)
+    {
+        color[i] = WHITE;
+        parent[i] = -1;
+        dist[i] = INT_MIN;
+    }
+    Queue b;
+    b.enqueue(source);
+    color[source] = GREY;
+    dist[source] = 0;
+    while(!b.empty())
+    {
+        int u = b.dequeue();
+        cout << u << " - ";
+        for(int i = 0;i<nVertices;i++)
+        {
+            int x = adjList[u].getItem(i);
+            if(color[x] == WHITE)
+            {
+                color[x] = GREY;
+                b.enqueue(x);
+                parent[x] = u;
+                dist[x] = dist[u] + 1;
+            }
+        }
+        color[u] = BLACK;
+    }
 
 }
 
@@ -285,7 +368,11 @@ int Graph::getDist(int u, int v)
 {
     //returns the shortest path distance from u to v
     //must call bfs using u as the source vertex, then use distance array to find the distance
-    return INFINITY ;
+    if(u<0 || v<0 || u>=nVertices || v>=nVertices)
+        //vertex out of range
+        return INFINITY ;
+    bfs(u);
+    return dist[v];
 }
 
 void Graph::printGraph()
@@ -305,6 +392,10 @@ void Graph::printGraph()
 Graph::~Graph()
 {
     //write your destructor here
+    delete[] adjList;
+    delete[] color;
+    delete[] parent;
+    delete[] dist;
 }
 
 
@@ -319,6 +410,7 @@ int main(void)
     bool dir;
     printf("Enter your choice:\n");
     printf("1. directed graph   2. undirected graph\n");
+    cout << "Enter your choice : ";
     scanf("%d",&choice);
     if(choice == 1)dir = true;
     else if(choice == 2)dir = false;
@@ -330,33 +422,120 @@ int main(void)
 
     while(1)
     {
-        printf("1. Add edge. \n");
-        printf("4. (Add from homework). 5. Print Graph  6. Exit.\n");
+        printf("1. Add edge\n2. Remove edge\n3. Check if there is an edge between two vertices\n4. Check the degree of a vertex\n");
+        printf("5. Print the adjacent vertices of a vertex\n6. Print Graph\n7. Check if two vertex have common adjacent vertices\n");
+        printf("8. Run bfs of a vertex\n9. Get the distance between two vertices\n10. Exit\n");
 
         int ch;
         scanf("%d",&ch);
         if(ch==1)
         {
             int u, v;
-            scanf("%d%d", &u, &v);
+            printf("Enter u and v : ");
+            scanf("%d %d", &u, &v);
             g.addEdge(u, v);
+            cout << endl;
+            //break;
         }
         else if(ch==2)
         {
+            int u, v;
+            printf("Enter u and v : ");
+            scanf("%d %d", &u, &v);
+            g.removeEdge(u,v);
+            cout << endl;
+           // break;
 
         }
         else if(ch==3)
         {
-
+            int u, v;
+            printf("Enter u and v : ");
+            scanf("%d %d", &u, &v);
+            if(g.isEdge(u,v))
+                cout << "There is an edge" << endl;
+            else
+                cout << "There is no edge" << endl;
+         }
+        else if(ch==4)
+        {
+            int u,val;
+            cout << "Enter the vertex to print its degree : ";
+            cin >> u;
+            if(choice==1)
+            {
+                int o;
+                cout << "Press 1 to get out degree and 2 to get in degree : ";
+                cin >> o;
+                if(o==1)
+                    val = g.getDegree(u,true);
+                else
+                    val = g.getDegree(u,false);
+            }
+            else
+                val = g.getDegree(u);
+            cout << "Degree : " << val;
+            cout << endl;
+           // break;
         }
         else if(ch==5)
         {
-            g.printGraph();
+            int u;
+            cout << "Enter the vertex to print its adjacent vertices : ";
+            cin >> u;
+            cout << "Adjacent vertices - ";
+            g.printAdjVertices(u);
+            cout << endl;
+            //break;
         }
+
         else if(ch==6)
         {
+            g.printGraph();
+            cout << endl;
+            //break;
+        }
+
+        else if(ch==7)
+        {
+            int u,v;
+            cout << "Enter the vertices : ";
+            scanf("%d %d",&u,&v);
+            if(g.hasCommonAdjacent(u,v))
+                cout << "They have common adjacent vertices";
+            else
+                cout << "They do not have common adjacent vertices";
+            cout << endl;
+            //break;
+        }
+
+        else if(ch==8)
+        {
+            int u;
+            cout << "Enter the source vertex : ";
+            cin >> u;
+            g.bfs(u);
+            cout << endl;
+            //break;
+        }
+
+        else if(ch==9)
+        {
+            int u,v;
+            cout <<"Enter the two vertices : ";
+            cin >> u >> v;
+            cout << "Distance between them = " << g.getDist(u,v);
+            cout << endl;
+            //break;
+        }
+
+        else if(ch==10)
+        {
+           // g.~Graph();
+            cout << endl;
             break;
         }
     }
+    return 0;
 
 }
